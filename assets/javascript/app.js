392215ee5485f0c196a3384f482b361be863042d
Alex Briody <alexbriody@gmail.com>
2019-09-08 11:23:55 -0400

    folders and files

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    var 
+    
+    $("#startButton").on("click", reset);
+
+    $("#crystal2").on("click", function() {
+        if () {
+        
+        }
+    });
+
+
+
+    function reset() {
+    }
+    
+}
