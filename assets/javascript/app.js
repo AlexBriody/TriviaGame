eb18d25c626ddb4aae48deb800501edfcae60625
Alex Briody <alexbriody@gmail.com>
2019-09-09 11:07:02 -0400

    adjustment to failure to answer timer; added explanatory notes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
 
-    var number = 60;
-    var i = -1;
-    var intervalId;
-    var winner = 0;
-    var loser = 0;
-    var clicked = false;
-    var questionsLeft;
-    var questionBank = [
+    var number = 60;//60 seconds
+    var i = -1;//because the starGame function has i++, which brings us to 0, referencing the first array position
+    var intervalId;//holds the setInterval so that it can be cleared
+    var winner = 0;//keeps count of the number of correct answers
+    var loser = 0;//keeps count of the number of incorrect answers
+    var questionsLeft;//keeps count of unanswered questions
+    var noAnswer;//holds the setTimeout function for seven seconds
+    var nextQuestion;//holds the setTimeout function for three seconds
+    var questionBank = [//an array of objects (questions with choices and answer)
         {
             question: "What was the name of the dog character in the TV series 'Frasier'?",
             choices: ["Jesse", "Eddie", "Lucky", "Milo"],
@@ -64,28 +65,23 @@ $(document).ready(function () {
 
     ]
 
-    questionsLeft = questionBank.length;
+    questionsLeft = questionBank.length;//the length of the array of objects (number of questions)
 
     function threeSeconds() {
         startGame();
     }
 
-    // function sevenSeconds() {
-    //         $("#row3,#row4,#row5").text("");
-    //         $("#row2").text("You failed to give an answer in time.");
-    //         setTimeout(threeSeconds, 3000);
-       
-    // }
+    function sevenSeconds() {
+            $("#row3,#row4,#row5").text("");
+            $("#row2").text("You failed to give an answer in time.");
+            nextQuestion = setTimeout(threeSeconds, 3000);//holds the setTimeout function so it can be cleared; and allows the failure message to be seen for 3 seconds
+    }
 
     function startGame() {
-        clicked = false;
-        // if (clicked === false && number !== 0) 
-        // {
-        // setTimeout(sevenSeconds, 7000);
-        // };
         if (number > 0) {
             i++;
             displayQuestion(i);
+            noAnswer = setTimeout (sevenSeconds, 7000);
         }
     }
 
@@ -98,35 +94,34 @@ $(document).ready(function () {
     }
 
     function endGame () {
+        clearTimeout(noAnswer);//clears 7 second timer
+        clearTimeout(nextQuestion);//clears 3 second timer
         $("#row1").text("Game Over!");
         $("#row2").text("Number of correct answers: " + winner);
         $("#row3").text("Number of incorrect answers: " + loser);
         $("#row4").text("Number of unanswered questions: " + questionsLeft);
         $("#row5").text("");
-        number = 60;
-        winner = 0;
-        loser = 0;
-        noAnswer = 0;
-        i = -1;
-        clicked = false;
-        questionsLeft = questionBank.length;
+        number = 60;//sets up for next game
+        winner = 0;//sets up for next game
+        loser = 0;//sets up for next game
+        noAnswer = 0;//sets up for next game
+        i = -1;//sets up for next game
+        questionsLeft = questionBank.length;//sets up for next game
     }
 
     $("#startButton").on("click", function () {
 
         function runTimer() {
             clearInterval(intervalId);
-            intervalId = setInterval(decrement, 1000);
+            intervalId = setInterval(decrement, 1000);//holds the setInterval so that it can be cleared
         }
 
         function decrement() {
             number--;
-
             $("#timeRemaining").text("Time Remaining: " + number + " seconds");
-
             if (number === 0) {
-                stop();
-                endGame();
+                stop();//clears the interval for the timer
+                endGame();//stops the current game and sets up for the next game
             }
         }
 
@@ -134,14 +129,10 @@ $(document).ready(function () {
             clearInterval(intervalId);
         }
 
-
-        runTimer();
-        startGame();
-
+        runTimer();//starts the timer
+        startGame();//starts the game
 
     });//closing for on.click function for start button
-
-
 
     $("#row2,#row3,#row4,#row5").on("click", function () {
 
@@ -157,31 +148,20 @@ $(document).ready(function () {
             $("#row3").append("The correct answer is " + questionBank[i].answer + ".");
             loser++;
         }
-
         
+        //compares the clicked answer to the question's answer and display correct or incorrect message
         if ($(this).text() === questionBank[i].answer) {
+            clearTimeout(noAnswer);//stops the 7 seconds countdown to display no response message
             displayWinnerMessage();
-            setTimeout(threeSeconds, 3000);
-            clicked = true;
+            nextQuestion = setTimeout(threeSeconds, 3000);//holds the setTimeout function so it can be cleared
             questionsLeft--;
         } else {
+            clearTimeout(noAnswer);//stops the 7 seconds countdown to display no response message
             displayLoserMessage();
-            setTimeout(threeSeconds, 3000);
-            clicked = true;
+            nextQuestion = setTimeout(threeSeconds, 3000);//holds the setTimeout function so it can be cleared
             questionsLeft--;
         };
 
-
     });//closing for on.click function for #row2,#row3,#row4,#row5
-
-
-
-
-
-
-
-
-
-
 
 })//document.ready ending bracket and parenthesis
