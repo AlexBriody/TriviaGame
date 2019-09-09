efb8d5110ac6a43f31d7f3169970ea8d82802f39
Alex Briody <alexbriody@gmail.com>
2019-09-08 21:49:35 -0400

    initial codes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,97 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    var 
-    
-    $("#startButton").on("click", reset);
+    var number = 30;
+    var i = -1;
+    var intervalId;
+    var gameStarted = false;
+    var questionBank = [
+        {
+            question: "What was the name of the dog character in the original 'Poltergeist' film?",
+            choices: ["Jaxon", "Buzzbie", "EBuzz", "Buddy"],
+            answer: "EBuzz"
+        },
 
-    $("#crystal2").on("click", function() {
-        if () {
-        
+        {
+            question: "What was the name of the dog character in the TV series 'Frasier'?",
+            choices: ["Jesse", "Eddie", "Lucky", "Milo"],
+            answer: "Eddie"
+        },
+
+        {
+            question: "What was the name of the dog character in the movie 'A Dog's Way Home'?",
+            choices: ["Bella", "Sadie", "Chloe", "Lola"],
+            answer: "Bella"
+        },
+    ]
+
+    $("#startButton").on("click", function () {
+
+        function run() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
+            gameStarted = true;
         }
-    });
+
+        function decrement() {
+            number--;
+
+            $("#timeRemaining").text("Time Remaining: " + number + " seconds");
+
+            if (number === 0) {
+                stop();
+            }
+        }
+
+        function stop() {
+            clearInterval(intervalId);
+            gameStarted = false;
+        }
+
+        function displayQuestion(i) {
+            $("#row1").text(questionBank[i].question);
+            $("#row2").text(questionBank[i].choices[0]);
+            $("#row3").text(questionBank[i].choices[1]);
+            $("#row4").text(questionBank[i].choices[2]);
+            $("#row5").text(questionBank[i].choices[3]);
+        }//function displayQuestion bracket
+
+        run();
+
+        if (gameStarted === true && number > 0) {
+            i++;
+            displayQuestion(i);
+        }
+
+    });//closing for on.click function for start button
+
+    $("#row2,#row3,#row4,#row5").on("click", function () {
+        
+        function displayWinnerMessage() {
+            $("#row3,#row4,#row5").text("");
+            $("#row2").text("Correct Answer!");
+        }
+
+        function displayLoserMessage() {
+            $("#row3,#row4,#row5").text("");
+            $("#row2").text("Incorrect Answer!");
+            $("#row3").append("The correct answer is " + questionBank[i].answer + ".");
+        }
+
+        if($(this).text() === questionBank[i].answer) {
+            displayWinnerMessage ();
+        }else {displayLoserMessage ()};
+
+
+    });//closing for on.click function for #row2,#row3,#row4,#row5
 
 
 
-    function reset() {
-    }
-    
-}
+
+
+
+
+
+
+
+
+})//document.ready ending bracket and parenthesis
